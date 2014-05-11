53dc99e92ffddd78d304a005c556ff449ac9d7f6
Michael Beckwith <tw2113@gmail.com>
2014-05-11 01:30:43 -0500

    add our bp-groups file

---
+++ includes/bp-groups.php
@@ -0,0 +1,17 @@
+<?php
+
+function badgeos_bp_group_submission_filters( $output, $atts ) {
+	$bp_public_groups = groups_get_groups();
+
+	if ( $bp_public_groups['total'] > 0 ) {
+		$output .= '<div class="badgeos-feedback-bp-groups">';
+		$output .= '<select name="badgeos_bp_groups" id="badgeos_bp_groups">';
+		foreach( $bp_public_groups['groups'] as $group ) {
+			$output .= '<option value="' . $group->id . '">' . $group->name . '</option>';
+		}
+		$output .= '</select>';
+		$output .= '</div>';
+	}
+	return $output;
+}
+add_filter( 'badgeos_render_feedback', 'badgeos_bp_group_submission_filters', 10, 2 );
