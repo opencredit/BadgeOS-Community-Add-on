645cc8251eaac0990773f7fc3ec710bf8aaa946c
Michael Beckwith <tw2113@gmail.com>
2014-05-13 11:55:27 -0500

    indent and wrap in our show_filter atts check

--- includes/bp-groups.php
+++ includes/bp-groups.php
@@ -1,17 +1,21 @@
 <?php
 
 function badgeos_bp_group_submission_filters( $output, $atts ) {
-	$bp_public_groups = groups_get_groups();
 
-	if ( $bp_public_groups['total'] > 0 ) {
-		$output .= '<div class="badgeos-feedback-bp-groups">';
-		$output .= '<select name="badgeos_bp_groups" id="badgeos_bp_groups">';
-		foreach( $bp_public_groups['groups'] as $group ) {
-			$output .= '<option value="' . $group->id . '">' . $group->name . '</option>';
+	if ( 'false' !== $atts['show_filter'] ) {
+		$bp_public_groups = groups_get_groups();
+
+		if ( $bp_public_groups['total'] > 0 ) {
+			$output .= '<div class="badgeos-feedback-bp-groups">';
+			$output .= '<select name="badgeos_bp_groups" id="badgeos_bp_groups">';
+			foreach( $bp_public_groups['groups'] as $group ) {
+				$output .= '<option value="' . $group->id . '">' . $group->name . '</option>';
+			}
+			$output .= '</select>';
+			$output .= '</div>';
 		}
-		$output .= '</select>';
-		$output .= '</div>';
 	}
+
 	return $output;
 }
 add_filter( 'badgeos_render_feedback', 'badgeos_bp_group_submission_filters', 10, 2 );
