2fa280d2e44b134e84d4a5f24a853ca0caedcae0
Brian Richards <brian@rzen.net>
2014-06-09 16:44:46 -0400

    Add filter for inserting BP Activity stream

--- includes/bp-activity.php
+++ includes/bp-activity.php
@@ -23,14 +23,16 @@ function badgeos_award_achievement_bp_activity( $user_id, $achievement_id, $this
 	$type = $post->post_type;
 
 	// Don't make activity posts for step post type
-	if ( 'step' == $type )
+	if ( 'step' == $type ) {
 		return false;
+	}
 
 	// Check if option is on/off
 	$achievement_type = get_page_by_title( str_replace('-',' ', $type), 'OBJECT', 'achievement-type' );
 	$can_bp_activity = get_post_meta( $achievement_type->ID, '_badgeos_create_bp_activty', true );
-	if ( ! $can_bp_activity )
+	if ( ! $can_bp_activity ) {
 		return false;
+	}
 
 	// Grab the singular name for our achievement type
 	$post_type_singular_name = strtolower( get_post_type_object( $type )->labels->singular_name );
@@ -45,7 +47,7 @@ function badgeos_award_achievement_bp_activity( $user_id, $achievement_id, $this
 	bp_activity_add( apply_filters(
 		'badgeos_award_achievement_bp_activity_details',
 		array(
-			'action'       => sprintf( __( '%1$s earned a %2$s: %3$s', 'badgeos-community' ), bp_core_get_userlink( $user_id ), $post_type_singular_name, '<a href="' . get_permalink( $achievement_id ) . '">' . $post->post_title . '</a>' ),
+			'action'       => sprintf( __( '%1$s earned the %2$s: %3$s', 'badgeos-community' ), bp_core_get_userlink( $user_id ), $post_type_singular_name, '<a href="' . get_permalink( $achievement_id ) . '">' . $post->post_title . '</a>' ),
 			'content'      => $content,
 			'component'    => 'badgeos',
 			'type'         => 'activity_update',
